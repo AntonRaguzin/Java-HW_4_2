6fa19e0ae0c200e01471f34d6eb7e11a552be942
antonraguzin <raguzin82@gmail.com>
2022-06-02 10:31:47 +0300

    Fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,17 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
+    public static void main(String[] args) {
 
-    long amount = 1000_60;
-    boolean registered = true;
+        BonusService service = new BonusService();
 
-    service.calculate(amount, registered);
-  }
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    private static void assertEquals(long expected, long actual) {
+    }
 }
-
